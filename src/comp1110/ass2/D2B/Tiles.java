0b7794bdb1563651ab47da7074394ca8998a1856
Ke Ning <u7175553@anu.edu.au>
2021-04-19 03:48:45 +0800

    pure clean

--- src/comp1110/ass2/D2B/Tiles.java
+++ src/comp1110/ass2/D2B/Tiles.java
@@ -1,5 +1,19 @@
 package comp1110.ass2.D2B;
 
+//Define a class Tiles that can be used by all the classes
 public enum Tiles {
-    Blue()
+    B("Blue", "a"),
+    G("Green", "b"),
+    O("Orange", "c"),
+    P("Purple", "d"),
+    R("Red", "e"),
+    FP("First Player", "f");
+    public String longName;
+    public String encode;
+
+    Tiles(String longName, String encode) {
+        this.longName = longName;
+        this.encode = encode;
+
+    }
 }
