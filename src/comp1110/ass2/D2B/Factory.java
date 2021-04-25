20c86e444e0bafe687670dc3c763f7de07569d71
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-04-25 20:46:15 +0530

    Some changes to Factory

--- src/comp1110/ass2/D2B/Factory.java
+++ src/comp1110/ass2/D2B/Factory.java
@@ -1,16 +1,38 @@
 package comp1110.ass2.D2B;
 
+/**
+ * @author Mukund Balaji Srinivas
+ * <p>Each instance of this class consists of two parts</p>
+ * <p>int Number referring to a particular factory and using number 0-8 </p>
+ * <p>Tiles[4] the tiles in each factory</p>
+ */
+
 public class Factory {
     public Tiles[] tiles = new Tiles[4];
     public int Number;
 
     /**
-     * Set the value of tiles to something that is m
-     * @param n
+     * @author Mukund Balaji Srinivas
+     * Set the address of each factory
+     * @param inP referring to each factory
      */
-    public void setFacNumber(int n){
-        this.Number=n;
+    public void setFacNumber(String inP){
+        String AddR=inP.
     }
+
+    /**
+     * @author Mukund Balaji Srinivas
+     * Takes string and splits it into all the
+     * @param tilesString
+     */
+    public void setFacTiles(String tilesString){
+        for(int i=0;i<tilesString.substring(1).length();i++)
+            for(Tiles Tile:Tiles.values())
+                if(tilesString.charAt(i) == Tile.symbol)
+                    tiles[i]=Tile;
+
+    }
+
 
 
 }
