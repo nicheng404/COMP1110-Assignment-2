9ea8119e2c671a78dc3bcebd2278110474cc097b
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-05-08 20:14:06 +0530

    Made some changes to Board and deleted Player and Turn

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,6 +1,7 @@
 package comp1110.ass2.gui;
 
 import comp1110.ass2.*;
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -8,7 +9,9 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
-public class Board extends Pane {
+import java.util.ArrayList;
+
+public class Board extends Application {
     private static final int BOARD_WIDTH = 1200;
     private static final int BOARD_HEIGHT = 700;
 
@@ -18,8 +21,8 @@ public class Board extends Pane {
         Tile(int colorVal, int x, int y) {
             setWidth(50);
             setHeight(50);
-            setLayoutX(x);
-            setLayoutY(y);
+            setX(x);
+            setY(y);
             setFill(Color.rgb(colorVal * 5, (colorVal * 5) + 50, (colorVal * 5) + 80));
             setStroke(Color.BLACK);
             setStrokeType(StrokeType.OUTSIDE);
@@ -29,27 +32,24 @@ public class Board extends Pane {
 
     class arrangeMosaic extends TilePane {
         public int nTiles;
-        public Tile[] tiles;
+        public ArrayList<Tile> tiles = new ArrayList<>();
 
         arrangeMosaic(int nTiles) {
             this.nTiles = nTiles;
-            this.tiles = new Tile[nTiles];
-            for (int i = 0; i < nTiles; i++) {
-                tiles[i] = new Tile(i, i + 5, i + 5);
-                this.getChildren().add(tiles[i]);
+            Tile TempTile;
+            for (int i = 1; i <= (int) Math.sqrt(nTiles); i++) {
+                for (int j = 1; j <= (int) Math.sqrt(nTiles); j++) {
+                    TempTile = new Tile(i, 50 * i, 50 * j);
+                    tiles.add(TempTile);
+                }
             }
-            this.setPrefColumns((int) Math.sqrt(nTiles));
+            this.getChildren().addAll(tiles);
         }
     }
+
     public void start(Stage stage){
         arrangeMosaic mosTiles = new arrangeMosaic(25);
-        AnchorPane ap = new AnchorPane();
-        ap.setLayoutX(100);
-        ap.setLayoutY(100);
-        ap.getChildren().add(mosTiles);
-        Scene scene = new Scene(ap,BOARD_WIDTH,BOARD_HEIGHT);
-        mosTiles.setPrefColumns(5);
-        mosTiles.setPrefColumns(5);
+        Scene scene = new Scene(mosTiles,BOARD_WIDTH,BOARD_HEIGHT);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
