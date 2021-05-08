293aad20d35017409e49ecb496c51fc20e2a48d7
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-05-08 18:11:48 +0530

    Made some more changes to the board

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -1,10 +1,12 @@
 package comp1110.ass2.gui;
 
 import comp1110.ass2.*;
+import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
+import javafx.stage.Stage;
 
 public class Board extends Pane {
     private static final int BOARD_WIDTH = 1200;
@@ -24,8 +26,33 @@ public class Board extends Pane {
         }
 
     }
-     class arrangeMosaic extends TilePane{
 
-     }
+    class arrangeMosaic extends TilePane {
+        public int nTiles;
+        public Tile[] tiles;
+
+        arrangeMosaic(int nTiles) {
+            this.nTiles = nTiles;
+            this.tiles = new Tile[nTiles];
+            for (int i = 0; i < nTiles; i++) {
+                tiles[i] = new Tile(i, i + 5, i + 5);
+                this.getChildren().add(tiles[i]);
+            }
+            this.setPrefColumns((int) Math.sqrt(nTiles));
+        }
+    }
+    public void start(Stage stage){
+        arrangeMosaic mosTiles = new arrangeMosaic(25);
+        AnchorPane ap = new AnchorPane();
+        ap.setLayoutX(100);
+        ap.setLayoutY(100);
+        ap.getChildren().add(mosTiles);
+        Scene scene = new Scene(ap,BOARD_WIDTH,BOARD_HEIGHT);
+        mosTiles.setPrefColumns(5);
+        mosTiles.setPrefColumns(5);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 
 }
