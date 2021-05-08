54979b1cf866817f07f93a441c88dce2b522e325
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2021-05-08 13:06:33 +0530

    Changes to mosaic null tiles

--- src/comp1110/ass2/Mosaic.java
+++ src/comp1110/ass2/Mosaic.java
@@ -7,64 +7,24 @@ public class Mosaic {
     public ArrayList<ArrayList<Tiles>> mosaic = new ArrayList<>();
     public boolean isValid;
     public String inString;
-    Mosaic(String inString){
-        this.inString=inString;
+
+    Mosaic(String inString) {
+        this.inString = inString;
         setMosaic();
     }
 
     public void setMosaic() {
+        //initialise all the mosaics to "*" in the beginning
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                mosaic.get(i).add(j, Tiles.E);
+            }
+        }
         //Get the address of these tiles and check if they are valid by checking for their lengths
         ArrayList<Integer> tileAddr = new ArrayList<>();
         for (int i = 0; i < inString.length(); i++) {
             if (inString.charAt(i) >= 'a' && inString.charAt(i) <= 'd') {
                 tileAddr.add(i);
-
-
-
-    public static final char HEAD = 'M';
-    public String mosaicTilesString; // mosaic string in state1 without 'M'
-
-
-
-
-
-
-
-
-    /**
-     * Check whether mosaic for a certain player is well formed with
-     *  a given certain player mosaic string in state1 without 'M'.
-     *
-     * @param mosaicTilesString mosaic string for a certain player in state1 without 'M'.
-     * @return true/false. whether mosaic for a certain player is well formed.
-     */
-
-    public static boolean mosaicTilesWellFormed(String mosaicTilesString) {
-
-
-        // mosaic -> char[]
-        int mosaicLength = mosaicTilesString.length();
-        char[] mosaicArray = new char[mosaicLength];
-        mosaicArray = mosaicTilesString.toCharArray();
-
-        //criteria : lengthIs3, mosaicChar1Well, mosaicChar2Well, mosaicChar3Well, mosaicOrderWell
-        boolean mosaiclengthIs3 = false;
-        boolean mosaicChar1Well = true;
-        boolean mosaicChar2Well = true;
-        boolean mosaicChar3Well = true;
-        boolean mosaicOrderWell = true;
-
-        //mosaic criteria 1 : check length 是3的倍数
-        if (mosaicLength % 3 == 0) {
-            mosaiclengthIs3 = true;
-
-            //mosaic criteria 2 :1st well
-            for (int i = 0; i < mosaicLength; i = i + 3) {
-                if (mosaicArray[i] == 'a' || mosaicArray[i] == 'b'
-                        || mosaicArray[i] == 'c' || mosaicArray[i] == 'd' || mosaicArray[i] == 'e') {
-                } else {
-                    mosaicChar1Well = false;
-                }
             }
         }
         boolean[] checkVal = new boolean[tileAddr.size() - 1];
@@ -76,7 +36,7 @@ public class Mosaic {
         boolean[] trueArray = new boolean[checkVal.length];
         Arrays.fill(trueArray, true);
         isValid = Arrays.equals(trueArray, checkVal);
-        int row , col ;
+        int row, col;
         if (isValid) {
             for (int i = 0; i < tileAddr.size(); i++) {
                 row = Integer.parseInt(inString.substring(tileAddr.get(i + 1), tileAddr.get(i + 2)));
@@ -91,20 +51,3 @@ public class Mosaic {
         }
     }
 }
-
-    }
-
-//[Ma02a13b00e42] -> rowNumber,color of tiles, number of tiles in the row.
-// arrange tiles to place in the storage
-//   public void arrangeStorageTo2DArray(String [] tiles){
-//       String [][] string_storage = new String [5] [5];
-//    }
-
-//        for(int i=0; i< tiles.length;i++){
-//            if(tiles)
-// }
-
-
-}
-
-
