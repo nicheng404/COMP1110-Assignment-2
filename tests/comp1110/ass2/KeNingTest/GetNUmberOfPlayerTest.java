e4d50bde3261ddff9aaaf46892d5eef66f1ad42c
Ke Ning <u7175553@anu.edu.au>
2021-05-20 15:50:28 +0800

    4

--- tests/comp1110/ass2/KeNingTest/GetNUmberOfPlayerTest.java
+++ tests/comp1110/ass2/KeNingTest/GetNUmberOfPlayerTest.java
@@ -11,28 +11,28 @@ public class GetNUmberOfPlayerTest {
     public void test2Players(){
         String totalPlayerState = "A0Me04b11S2c13a34a1FB1Mc02d33S1b12e1F";
         int numberOfPlayer = 2;
-        assertTrue(numberOfPlayer== Player.getNUmberOfPlayer(totalPlayerState));
+        assertTrue(numberOfPlayer== Player.getNumberOfPlayer(totalPlayerState));
     }
 
     @Test
     public void test1Players(){
         String totalPlayerState = "A0Me04b11S2c13a34a1F";
         int numberOfPlayer = 1;
-        assertTrue(numberOfPlayer== Player.getNUmberOfPlayer(totalPlayerState));
+        assertTrue(numberOfPlayer== Player.getNumberOfPlayer(totalPlayerState));
     }
 
     @Test
     public void test0Players(){
         String totalPlayerState = "";
         int numberOfPlayer = 0;
-        assertTrue(numberOfPlayer== Player.getNUmberOfPlayer(totalPlayerState));
+        assertTrue(numberOfPlayer== Player.getNumberOfPlayer(totalPlayerState));
     }
 
     @Test
     public void testExtraPlayers(){
         String totalPlayerState = "A0Me04b11S2c13a34a1FB1Mc02d33S1b12e1FC0Me04b11S2c13a34a1FD1Mc02d33S1b12e1FE1Mc02d33S1b12e1F";
         int numberOfPlayer = 999;
-        assertTrue(numberOfPlayer== Player.getNUmberOfPlayer(totalPlayerState));
+        assertTrue(numberOfPlayer== Player.getNumberOfPlayer(totalPlayerState));
     }
 
 
